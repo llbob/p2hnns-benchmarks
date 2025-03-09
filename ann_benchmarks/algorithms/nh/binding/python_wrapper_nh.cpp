bfa28c7d8842d96ff5e90d6ea0fd68f475c44e54
llbob <miniporse@gmail.com>
2025-03-09 16:27:35 +0100

    Add nh wrapper - currently making an error msg 139 in search phase, consider the signature of query?

---
+++ ann_benchmarks/algorithms/nh/binding/python_wrapper_nh.cpp
@@ -0,0 +1,58 @@
+#include <nh.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "util.h"
+
+namespace py = pybind11;
+
+namespace p2h
+{
+    // int   n,                            // number of input data
+    // int   d,                            // dimension of input data
+    // int   m,                            // #hashers
+    // int   s,                            // scale factor of dimension
+    // float w,                            // bucket width
+    // const DType *data)                  // input data
+	class WrapperNH
+	{
+	public:
+		std::unique_ptr<NH<float>> nh;
+
+		void preprocess(int n, int d, int m, int s, float w, py::array_t<float> data)
+		{
+			py::buffer_info buf = data.request();
+			float* ptr = static_cast<float*>(buf.ptr);
+			nh.reset(new NH<float>(n, d, m, s, w, ptr));
+		}
+		// int   top_k,                        // top-k value
+		// int   cand,                         // #candidates
+		// const float *query,                 // input query
+		// MinK_List *list)                    // top-k results (return)
+
+		std::vector<int> search(int top_k, int cand, py::array_t<float> query)
+		{
+			py::buffer_info buf = query.request();
+			float* ptr = static_cast<float*>(buf.ptr);
+			
+			MinK_List list(top_k);
+			nh->nns(top_k, cand, ptr, &list);
+
+			std::vector<int> return_list;
+			for (int i = 0; i < list.size(); ++i)
+			{
+				// we need to subtract 1 because the ids are 1-indexed in the C++ code
+				return_list.push_back(list.ith_id(i)-1);
+			}
+			return return_list;
+		}
+	};
+
+	PYBIND11_MODULE(nh, m)
+	{
+		py::class_<WrapperNH>(m, "NH")
+			.def(py::init<>())
+			.def("preprocess", &WrapperNH::preprocess)
+			.def("search", &WrapperNH::search);
+	}
+}
---
+++ ann_benchmarks/algorithms/nh/binding/setup.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+try:
+    from setuptools import setup, find_packages, Extension
+except ImportError:
+    sys.stderr.write('Setuptools not found!\n')
+    raise
+
+extra_args = ['-std=c++17', '-march=native', '-O3']
+extra_link_args = ['-ltbb']
+
+if sys.platform != 'darwin':
+    extra_args += ['-fopenmp']
+    extra_link_args += ['-fopenmp']
+else:
+    extra_args += ['-mmacosx-version-min=10.9', '-stdlib=libc++', '-Xclang', '-fopenmp']
+    extra_link_args += ['-lomp']
+    os.environ['LDFLAGS'] = '-mmacosx-version-min=10.9'
+
+module = Extension(
+    'nh',
+    sources=['python_wrapper_nh.cpp', 'methods/pri_queue.cc', 'methods/util.cc'], 
+    library_dirs=["methods/"],
+    extra_compile_args=extra_args,
+    extra_link_args=extra_link_args,
+    runtime_library_dirs=['methods/'],
+    include_dirs=[
+        'methods/', 
+        'external/pybind11/include', 
+        'libs',
+        './',  # Add root directory to include path
+        'lccs_bucket/',  # Add lccs_bucket directory
+        'lccs_bucket/bucketAlg/'  # Add the specific directory containing lcs_int.h
+    ])
+
+setup(
+    name='nh',
+    version='0.1',
+    description='NH Implementation',
+    ext_modules=[module]
+)
